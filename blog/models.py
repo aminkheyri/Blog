b2260e25cb86e24ffdcda9ff74e75bbd67f726b9
amin <kheyrriamin@yahoo.com>
2021-03-26 23:05:37 +0430

    model manager and navbar

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,11 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 
 
+class PublishedArticleManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status='publish')
+
+
 class Article(models.Model):
     STATUS = (
         ('draft', 'Draft'),
@@ -17,6 +22,8 @@ class Article(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=20, choices=STATUS, default='draft')
+    objects = models.Manager()
+    published = PublishedArticleManager()
 
     def __str__(self):
         return self.title
