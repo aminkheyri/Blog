766b45933a12a532ba0b2b1fca36e93162c5f375
amin <kheyrriamin@yahoo.com>
2021-03-27 01:26:25 +0430

    Login

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import UserLoginForm
+from django.contrib.auth import login, authenticate
 
 
 def user_login(request):
@@ -8,8 +9,10 @@ def user_login(request):
         if form.is_valid():
             username = form.cleaned_data['user_name']
             password = form.cleaned_data['password']
-            print(username)
-            print(password)
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('blog:all_article')
     else:
         form = UserLoginForm()
     return render(request, 'accounts/login.html', {'form': form})
