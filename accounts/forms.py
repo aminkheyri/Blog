f756579365a5abf0529bcfda4963bc0d0a3d778e
amin <kheyrriamin@yahoo.com>
2021-03-27 19:37:08 +0430

    password validation

--- accounts/forms.py
+++ accounts/forms.py
@@ -13,8 +13,10 @@ class UserRegistrationForm(forms.Form):
         attrs={'class': 'form-control', 'placeholder': 'Enter Your Username'}))
     email = forms.EmailField(max_length=50, widget=forms.EmailInput(
         attrs={'class': 'form-control', 'placeholder': 'Enter Your Email'}))
-    password = forms.CharField(max_length=50, widget=forms.PasswordInput(
+    password1 = forms.CharField(label='password', max_length=50, widget=forms.PasswordInput(
         attrs={'class': 'form-control', 'placeholder': 'Enter Your Password'}))
+    password2 = forms.CharField(label='password conformation', max_length=50, widget=forms.PasswordInput(
+        attrs={'class': 'form-control', 'placeholder': 'Confirm Your Password'}))
 
     def clean_email(self):
         email = self.cleaned_data['email']
@@ -22,3 +24,17 @@ class UserRegistrationForm(forms.Form):
         if user.exists():
             raise forms.ValidationError('The email is already exists')
         return email
+
+    # def clean_password2(self):
+    #     p1 = self.cleaned_data['password1']
+    #     p2 = self.cleaned_data['password2']
+    #     if p1 !=p2:
+    #         raise forms.ValidationError("Your Passwords must match")
+
+    def clean(self):
+        cleaned_data = super().clean()
+        p1 = cleaned_data['password1']
+        p2 = cleaned_data['password2']
+        if p1 and p2:
+            if p1 != p2:
+                raise forms.ValidationError('Your Passwords must match')
--- accounts/views.py
+++ accounts/views.py
@@ -28,7 +28,7 @@ def user_register(request):
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            User.objects.create_user(cd['user_name'], cd['email'], cd['password'])
+            User.objects.create_user(cd['user_name'], cd['email'], cd['password1'])
             messages.success(request, 'Congrats You Registered successfully Now Please log in')
             return redirect('accounts:user_login')
     else:
