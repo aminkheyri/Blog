719fd398e7a80e9293010e0f40355c989d4a4fcb
amin <kheyrriamin@yahoo.com>
2021-03-27 14:22:41 +0430

    messages framework widget

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,5 +2,7 @@ from django import forms
 
 
 class UserLoginForm(forms.Form):
-    user_name = forms.CharField(max_length=30)
-    password = forms.CharField(max_length=50)
+    user_name = forms.CharField(max_length=30, widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'Enter Your Username'}))
+    password = forms.CharField(max_length=50, widget=forms.PasswordInput(
+        attrs={'class': 'form-control', 'placeholder': 'Enter Your Password'}))
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from .forms import UserLoginForm
 from django.contrib.auth import login, authenticate
+from django.contrib import messages
 
 
 def user_login(request):
@@ -12,7 +13,10 @@ def user_login(request):
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
+                messages.success(request, 'congrats you have logged in to your account ', 'success')
                 return redirect('blog:all_article')
+            else:
+                messages.error(request, 'Wrong username or password', 'danger')
     else:
         form = UserLoginForm()
     return render(request, 'accounts/login.html', {'form': form})
